364b54f3a9f2efcac11b3a8fa27d0081742c4759
M616 <marcosmay@gmail.com>
2025-11-13 11:35:59 -0300

    agrego proyecciones caba censo 2022

---
+++ proyecciones_censo_2022.R
@@ -0,0 +1,27 @@
+library(httr)
+library(tidyverse)
+library(readxl)
+
+
+url <- "https://www.indec.gob.ar/ftp/cuadros/poblacion/proyecciones_jurisdicciones_2022_2040_c1.xlsx"
+
+dir.create('data')
+dir.create('data/censo_2022')
+archivo_local <- "data/censo_2022/proyecciones_poblacion_INDEC.xlsx"
+
+response <- GET(url)
+
+# Verificar que la petición fue exitosa
+if(status_code(response) == 200) {
+  # Guardar el contenido
+  writeBin(content(response, "raw"), archivo_local)
+  message("Archivo descargado exitosamente: ", archivo_local)
+} else {
+  message("Error en la descarga. Código de estado: ", status_code(response))
+}
+
+
+proyecciones <- read_excel("data/censo_2022/proyecciones_poblacion_INDEC.xlsx", 
+                                           sheet = "02-CABA", range = "a5:d24")
+
+caba_2025 <- proyecciones[proyecciones$Año == 2025, 2][[1]]
