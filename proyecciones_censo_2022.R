a03968a95910fca0e6f1f31040d153d8cd896c01
M616 <marcosmay@gmail.com>
2025-12-16 11:27:25 -0300

    agrego proporcion 1.5 proyeccion caba

--- proyecciones_censo_2022.R
+++ proyecciones_censo_2022.R
@@ -39,4 +39,7 @@ caba_2025 <- proyecciones[proyecciones$Año == 2025, 2][[1]]
 ## esto tomando 0.6 % (todos somos todos, pagina33)
 0.006 * caba_2025
 
+## esto tomando 1.5 según escenario conservador calculado a partir
+#de eah2018
+0.015 * caba_2025
 
