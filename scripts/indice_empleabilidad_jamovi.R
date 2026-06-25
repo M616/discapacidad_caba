9bd75eb7d325df006cd666b6d98b7affba7903b8
M616 <marcosmay@gmail.com>
2026-06-25 12:46:39 -0300

    d

--- scripts/indice_empleabilidad_jamovi.R
+++ scripts/indice_empleabilidad_jamovi.R
@@ -5,13 +5,8 @@ library(scales)
 
 jamovi <- read_omv("data/empleabilidad/Base Empleabilidad JAMOVI.omv")
 attributes(jamovi$IndiceEmpleabilidad)
-library(dplyr)
 
-tibble(
-  variable = names(jamovi),
-  clase = sapply(datos, \(x) class(x)[1]),
-  atributos = sapply(datos, \(x) paste(names(attributes(x)), collapse = ", "))
-)
+
 
 attributes(jamovi$Capital)
 
@@ -36,12 +31,66 @@ datos |>
 
 #Teniendo en cuenta el cortre etario, en la base original el 32 % de las personas no trabaja
 
-ocup <- datos |> filter(condicion_de_actividad == 'Trabaja')
-summary(ocup$indice_empleabilidad )
-summary(datos$indice_empleabilidad )
-boxplot(ocup$indice_empleabilidad )
+datos |>
+  filter(condicion_de_actividad == "Trabaja" & !is.na(indice_empleabilidad)) |>
+  summarise(
+    minimo = min(indice_empleabilidad),
+    p25 = quantile(indice_empleabilidad, 0.25),
+    mediana = median(indice_empleabilidad),
+    p75 = quantile(indice_empleabilidad, 0.75),
+    media = mean(indice_empleabilidad),
+    maximo = max(indice_empleabilidad))
 
-ocup |> select(alfabetizacion,tipo_de_vivienda, indice_empleabilidad)
+
+datos |>
+  filter(!is.na(indice_empleabilidad)) |>
+  group_by(condicion_de_actividad) |>
+  summarise(media_indice = mean(indice_empleabilidad))
 
 
+datos <- datos |>
+  mutate(
+    categoria_indice = case_when(
+      indice_empleabilidad < 5 ~ "Bajo",
+      indice_empleabilidad >= 5 & indice_empleabilidad <= 6 ~ "Medio",
+      indice_empleabilidad > 6 ~ "Alto"
+    )  )
 
+datos |>
+  filter(condicion_de_actividad %in% c("Trabaja", "No trabaja")) |>
+  count(categoria_indice, condicion_de_actividad) |>
+  group_by(categoria_indice) |>
+  mutate(porcentaje = n / sum(n) * 100)
+
+
+datos |>
+  filter(condicion_de_actividad %in% c("Trabaja", "No trabaja")) |>
+  mutate(trabaja_bin = if_else(condicion_de_actividad == "Trabaja", 1, 0)) |>
+  group_by(categoria_indice) |>
+  summarise(
+    prob_trabajar = mean(trabaja_bin)
+  )
+
+
+datos |>
+  filter(condicion_de_actividad %in% c("Trabaja", "No trabaja")) |>
+  mutate(trabaja_bin = if_else(condicion_de_actividad == "Trabaja", 1, 0)) |>
+  group_by(indice_empleabilidad) |>
+  summarise(prob_trabajar = mean(trabaja_bin))
+
+datos |>
+  filter(condicion_de_actividad %in% c("Trabaja", "No trabaja")) |>
+  mutate(trabaja_bin = if_else(condicion_de_actividad == "Trabaja", 1, 0)) |>
+  group_by(indice_empleabilidad) |>
+  summarise(prob_trabajar = mean(trabaja_bin)) |>
+  ggplot(aes(x = indice_empleabilidad, y = prob_trabajar)) +
+  geom_line() +
+  geom_point() +
+  labs(
+    x = "Índice de empleabilidad",
+    y = "Probabilidad de trabajar"
+  )
+
+
+lm(indice_empleabilidad ~ autonomia + severidad_clinica + capital, data = datos)
+lm(indice_empleabilidad ~ autonomia_inversa + severidad_clinica_inverso + capital, data = datos)
